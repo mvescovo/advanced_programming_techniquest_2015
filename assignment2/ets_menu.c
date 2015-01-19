391786c303a54aac547edceb99f32551fa8e7af9
Michael Vescovo <s3459317@student.rmit.edu.au>
2015-01-19 14:06:32 +1100

    added assignment2 files

---
+++ assignment2/ets_menu.c
@@ -0,0 +1,20 @@
+/***********************************************************************
+ * COSC1076 - Advanced Programming Techniques
+ * Summer 2015 Assignment #2
+ * Full Name        : EDIT HERE
+ * Student Number   : EDIT HERE
+ * Course Code      : EDIT HERE
+ * Program Code     : EDIT HERE
+ * Start up code provided by David Shaw
+ * Based on 2014 code by Paul Miller and Virginia King
+ **********************************************************************/
+#include "ets_menu.h"
+
+/* initialises the menu */
+void menu_init(struct menu_item * menu)
+{
+  /* The UNUSED() function is designed to prevent warnings while your 
+     * code is only partially complete. Delete this function call once 
+     * you are using your own code */
+    UNUSED(menu);
+}
---
+++ assignment2/ets_options.h
@@ -0,0 +1,25 @@
+/***********************************************************************
+ * COSC1076 - Advanced Programming Techniques
+ * Summer 2015 Assignment #2
+ * Full Name        : EDIT HERE
+ * Student Number   : EDIT HERE
+ * Course Code      : EDIT HERE
+ * Program Code     : EDIT HERE
+ * Start up code provided by David Shaw
+ * Based on 2014 code by Paul Miller and Virginia King
+ **********************************************************************/
+#include "ets_type.h"
+#include "ets_utility.h"
+
+#ifndef ETS_OPTIONS
+#define ETS_OPTIONS
+
+/* This an example of one of the menu functions.
+ * You will need to write one for each option.
+ * Each one can only take one argument, a pointer to struct ets
+ * Each returns a BOOLEAN, which indicates a success or not
+ */
+BOOLEAN display_equipment(struct ets *);
+/* you will need to define many more! */
+
+#endif
